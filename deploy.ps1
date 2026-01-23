a502faeb6053a690eeacd3217b7f95a9c2d49065
Mikachu2333 <linkchou@yandex.com>
2026-01-23 16:12:14 +0800

    push to lmx's repo

--- deploy.ps1
+++ deploy.ps1
@@ -58,6 +58,13 @@ try {
         # Push local master branch to remote gh-pages branch
         Exec { git push -f $repoUrl master:gh-pages } "Git push failed"
 
+        # PUSH to lmx's repo
+        $repoUrl = "https://github.com/lmx050218/sdsmu_welcome-web"
+        Write-Host "Pushing to gh-pages branch of $repoUrl..." -ForegroundColor Cyan
+
+        # Push local master branch to remote gh-pages branch
+        Exec { git push -f $repoUrl master:NEW } "Git push failed"
+
         Write-Host "Deployment complete successfully!" -ForegroundColor Green
     }
     finally {
